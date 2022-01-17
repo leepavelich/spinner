a8069cf7aef83041d9fee963dc477e127292cacc
Lee Pavelich <leepavelich@gmail.com>
2022-01-17 20:17:40 +0000

    Added some explainer comments

--- spinner2.js
+++ spinner2.js
@@ -1,5 +1,5 @@
-const animations = ['|', '/', '-', '\\']
-const FULL_REVS = 1;
+const animations = ['|', '/', '-', '\\']  // spinner animation frames
+const FULL_REVS = 1;  // number of full revolutions to animate
 for (let i = 0; i < 2 * FULL_REVS * animations.length + 1; i++) {
   setTimeout(() => {
     process.stdout.write(`\r${animations[i % animations.length]}   `);
