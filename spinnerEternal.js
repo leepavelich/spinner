79eb3e8067c1ec1edd79b1d74c3aeebba2165e53
Lee Pavelich <leepavelich@gmail.com>
2022-01-18 22:10:51 +0000

    Removed ability to include different callbacks (YAGNI)

--- spinnerEternal.js
+++ spinnerEternal.js
@@ -1,13 +1,13 @@
 const animations = ['|', '/', '-', '\\'];  // spinner animation frames
 const delay = 100;
 
-const rotate = (frame, callback, stopFrame = Infinity) => {
+const rotate = (frame, stopFrame = Infinity) => {
   if (frame >= stopFrame) return;
   setTimeout(() => {
     process.stdout.write(`\r${animations[frame % animations.length]}   `);
-    callback(++frame, callback, stopFrame);
+    rotate(++frame, stopFrame);
   }, delay);
 };
 
-// rotate(0, rotate);
-rotate(0, rotate, 8);
+rotate(0);
+// rotate(0, 8);
